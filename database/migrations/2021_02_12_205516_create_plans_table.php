f112aad2b57f2e5c161049d92a28f72b488417b7
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2021-02-12 15:59:46 -0500

    Primera Configuración, creación de los planes

---
+++ database/migrations/2021_02_12_205516_create_plans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug');
+            $table->string('currency')->default('cop');
+            $table->string('interval')->default('month');
+            $table->string('product');
+            $table->string('nickname')->unique();
+            $table->float('amount');
+            $table->boolean('s3')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
