6b7cdd54a366c78be5fcaadb65b1781f2cda3b37
Ubuntu <ubuntu@ip-172-31-39-146.us-east-2.compute.internal>
2021-02-12 21:19:25 +0000

    Se agrego el codigo system y tenancy en el archivo config.database.php

--- config/database.php
+++ config/database.php
@@ -63,6 +63,36 @@ return [
             ]) : [],
         ],
 
+        'system' => [
+            'driver' => 'mysql',
+            'host' => env('TENANCY_HOST', '127.0.0.1'),
+            'port' => env('TENANCY_PORT', '3306'),
+            'database' => env('TENANCY_DATABASE', 'tenancy'),
+            'username' => env('TENANCY_USERNAME', 'tenancy'),
+            'password' => env('TENANCY_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
+	'tenant' => [
+        'driver' => env('DB_DRIVER', 'mysql'),
+        'host' => '',
+        'port' => env('DB_PORT', '3306'),
+        'database' => '',
+        'username' => '',
+        'password' => '',
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'strict' => true,
+        'engine' => null,
+    ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
