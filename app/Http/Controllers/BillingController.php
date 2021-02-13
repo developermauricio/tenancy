5e4f52c36985de5d3aadd8c2066cb49c69104904
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2021-02-13 12:43:42 -0500

    Se agrega info al controlador de agregar tarjeta

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class BillingController extends Controller
 {
-    //
+    public function creditCardForm() {
+        return view("system.credit_card_form");
+    }
+
+    public function processCreditCardForm() {
+        $this->validate(request(), [
+            'card_number' => 'required',
+            'card_exp_year' => 'required',
+            'card_exp_month' => 'required',
+            'cvc' => 'required'
+        ]);
+
+        try {
+            DB::beginTransaction();
+            \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+
+            if ( ! auth()->user()->hasPaymentMethod()) {
+                auth()->user()->createAsStripeCustomer();
+            }
+
+            $paymentMethod = \Stripe\PaymentMethod::create([
+                'type' => 'card',
+                'card' => [
+                    'number' => request('card_number'),
+                    'exp_month' => request('card_exp_month'),
+                    'exp_year' => request('card_exp_year'),
+                    'cvc' => request('cvc'),
+                ]
+            ]);
+            auth()->user()->updateDefaultPaymentMethod($paymentMethod->id);
+            auth()->user()->save();
+            DB::commit();
+            return back()->with('message', ['success', __('Tarjeta actualizada correctamente')]);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return back()->with('message', ['danger', $exception->getMessage()]);
+        }
+    }
 }
