ce27a8201ceb25e12e5f2718e303846491a008ae
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2021-02-13 12:14:30 -0500

    seed para los usuarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(\App\User::class, 1)->create([
+            "name" => "Admin",
+            "email" => "admin@admin.co",
+            "role" => "admin",
+            "password" => bcrypt("password")
+        ]);
+
+        factory(\App\User::class, 1)->create([
+            "name" => "Cliente Inquilino",
+            "email" => "app@cliente.co",
+            "role" => "tenancy",
+            "password" => bcrypt("password")
+        ]);
     }
 }
