7e0f1132465ba74a8e94bf7ddcd6486847537891
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2021-02-13 12:03:58 -0500

    Solución problema de authserviceprovider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Hyn\Tenancy\Environment;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -24,6 +25,12 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        $hostname = app(Environment::class)->hostname();
+        if ($hostname){
+            view()->share('currentWebsite', $hostname->website);
+        }else{
+            view()->share('currentWebsite', null);
+        }
 
         //
     }
